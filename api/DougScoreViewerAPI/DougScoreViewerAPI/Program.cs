d600f7a34e1c37a23b815902031379d8cba0102a
jamesschuler <jameshschuler1@gmail.com>
2022-02-24 22:08:12 -0800

    Testing cors

--- api/DougScoreViewerAPI/DougScoreViewerAPI/Program.cs
+++ api/DougScoreViewerAPI/DougScoreViewerAPI/Program.cs
@@ -46,7 +46,6 @@ app.UseCors(policy => policy
     .SetIsOriginAllowed(origin => true)
     .AllowAnyMethod()
     .AllowAnyHeader()
-    .WithOrigins("http://localhost:3000/", "https://doug-score-viewer.vercel.app")
     .AllowCredentials());
 
 app.UseMiddleware<ErrorHandlerMiddleware>();
