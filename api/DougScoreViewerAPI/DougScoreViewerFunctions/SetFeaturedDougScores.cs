94be24759ed795d867f23e87bb8003faadf47020
jamesschuler <jameshschuler1@gmail.com>
2022-02-24 20:46:07 -0800

    Added Azure Function to call endpoint to set Featured DougScores

---
+++ api/DougScoreViewerAPI/DougScoreViewerFunctions/SetFeaturedDougScores.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+
+namespace DougScoreViewer.DougScoreViewerFunctions;
+
+public static class SetFeaturedDougScores
+{
+    [FunctionName("SetFeaturedDougScores")]
+    public static async Task RunAsync([TimerTrigger("0 0 10 * * *")] TimerInfo myTimer, ILogger log)
+    {
+        log.LogInformation($"C# Timer trigger function executed at: {DateTime.UtcNow}");
+
+        try
+        {
+            var client = new HttpClient();
+            var requestUri = Environment.GetEnvironmentVariable("SetFeaturedDougScoresEndpoint");
+            var response = await client.SendAsync(new HttpRequestMessage(HttpMethod.Post, requestUri));
+
+            if (response.IsSuccessStatusCode)
+            {
+                log.LogInformation("Success!");
+            }
+            else
+            {
+                log.LogError("API responded with status code: " + response.StatusCode.ToString());
+            }
+        }
+        catch (Exception ex)
+        {
+            log.LogError("Error Occurred!" + ex.Message);
+        }
+    }
+}
